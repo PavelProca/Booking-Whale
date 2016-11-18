dbe8b398bc2e6b059ca5f22a44dc493465cca383
Proca Pavel <proka.pavel@gmail.com>
2016-11-18 18:56:11 +0200

    Re-made dropdown

--- script.js
+++
@@ -1,4 +0,0 @@
-function change() {
-	document.getElementById("mob-dropdown1").classList.toggle('hidden');
-	document.getElementById("mob-dropdown2").classList.toggle('hidden');
-}
---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+var i = 0;
+function change() {
+	document.getElementById("mob-dropdown1").classList.toggle('hidden');
+	document.getElementById("mob-dropdown2").classList.toggle('hidden');
+}
+function showdrop() {
+	document.getElementById("statedrop").classList.toggle('clicked');
+}
+$(function() {
+  $('.dropdown ul li').on('click', function() {
+    var label = $(this).parent().parent().children('label');
+    label.attr('data-value', $(this).attr('data-value'));
+    label.html($(this).html());
+
+    $(this).parent().children('.selected').removeClass('selected');
+    $(this).addClass('selected');
+    while (i<1) {
+		document.getElementById("statedrop").classList.toggle('chosen');
+		i=5;
+    }
+  });
+});
