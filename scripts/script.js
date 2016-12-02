ac9a455afcec1b865f8cd56381c7062978afb8d9
Proca Pavel <proka.pavel@gmail.com>
2016-12-02 18:17:42 +0200

    made errors

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,4 @@
 var i = 0;
-function change() {
-	document.getElementById("mob-dropdown1").classList.toggle('hidden');
-	document.getElementById("mob-dropdown2").classList.toggle('hidden');
-}
 $(function() {
   $('.dropdown ul li').on('click', function() {
     var label = $(this).parent().parent().children('label');
@@ -12,12 +8,46 @@ $(function() {
     $(this).parent().children('.selected').removeClass('selected');
     $(this).addClass('selected');
     while (i<1) {
-		  document.getElementById("statedrop").classList.toggle('chosen');
-		  i=5;
+      document.getElementById("statedrop").classList.toggle('chosen');
+      i=5;
     }
   });
   
   $('.dropdown').on('click', function() {
-  	$(this).toggleClass('open');
+    $(this).toggleClass('open');
   });
 });
+function change() {
+	document.getElementById("mob-dropdown1").classList.toggle('hidden');
+	document.getElementById("mob-dropdown2").classList.toggle('hidden');
+}
+function globError() {
+  document.getElementById("error-block").classList.toggle('error');
+}
+
+/*
+var form  = document.getElementsByTagName('form')[0];
+var field = document.querySelectorAll('input[class^="loc-"]');
+var error = document.querySelectorAll('.error');
+
+form.addEventListener("submit", function (event) {
+  for (var i=0; i < 3; i++) {
+    if (field[i].value == 0) {
+      error[i].className = "error active";
+      event.preventDefault();
+    }
+  }
+}, false);
+*/
+var form  = document.getElementsByTagName('form')[0];
+var field = document.querySelectorAll('input[name="req-field"]');
+var error = document.querySelectorAll('.loc-error');
+
+form.addEventListener("submit", function (event) {
+  for (var i=0; i < 3; i++) {
+    if (field[i].value == 0) {
+      error[i].className = "loc-error active";
+      event.preventDefault();
+    }
+  }
+}, false);
